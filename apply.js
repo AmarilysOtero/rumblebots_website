7c2220391a13ca384880f434746ab828b8c255bf
Amarilys <46731790+AmarilysOtero@users.noreply.github.com>
2022-04-12 21:02:20 -0400

    Funtionality with JS.

---
+++ apply.js
@@ -0,0 +1,7 @@
+/* When the user clicks on the button,
+it goes to the apply page */
+function goToForm() {
+  document.getElementById("btn3").onclick = function () {
+         location.href = "apply.html";
+     };
+}
